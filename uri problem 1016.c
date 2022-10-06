06554153169ec5d7b4ea5883008428b8979ac080
mehedi37m <mz.mehedi37@gmail.com>
2022-10-06 14:36:20 +0600

    day 4

---
+++ uri problem 1016.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int s, hours, minutes, seconds;
+    scanf("%d",&s);
+
+    hours = s/3600;
+    s = s%3600;
+    minutes = s /60;
+    seconds = s%60;
+
+    printf("%d:%d:%d\n", hours, minutes, seconds);
+
+    return 0;
+}
+
